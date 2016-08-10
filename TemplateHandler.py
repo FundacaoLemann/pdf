871e0e3f0ea18318da23c545e4ca817fdca18ce3
Wesley Batista <wesleybatistasantos@gmail.com>
2016-08-10 00:27:01 -0300

    Update 'TemplateHandler.py'
    dummy data

--- TemplateHandler.py
+++ TemplateHandler.py
@@ -33,22 +33,35 @@ class TemplateHandler(object):
         return template
 
     def main(self, *args, **kwargs):
-        template = self._get_template('barchart.html')
+        template = self._get_template('default.html')
         template_data = {
             "former": {
                 "name": "Judite Aparecida",
                 "email": "judite.aparecida@email.com"
             },
             "created_at": datetime.now(),
-            "REPORT_NAME": "Relatorio Semanal",
+            "REPORT_NAME": "Relatorio Semanal Khan",
             "DATE_INI": "2016-07-01",
             "DATE_END": "2016-08-01",
-            "CHART_NAME": "% de alunos que acessaram a plataforma",
-            "IMG": {
-                "src": "{TMPL_DIR}/assets/sample/output.png".format(TMPL_DIR=self.TEMPLATE_DIR),
-                "width": "100%",
-                "title": "Numero de alunos que acessaram a plataforma",
-            }
+            "charts": [
+                {
+                    "title": "% de alunos que acessaram a plataforma",
+                    "src": "{TMPL_DIR}/assets/sample/output.png".format(TMPL_DIR=self.TEMPLATE_DIR),
+                    "width": "100%",
+                },
+                {
+                    "title": "lorem ipsum bl a bklas",
+                    "src": "{TMPL_DIR}/assets/sample/output.png".format(TMPL_DIR=self.TEMPLATE_DIR),
+                    "width": "100%",
+                }
+            ],
+            "tables": [
+                {
+                    "title": "Lorem Some cool title",
+                    'meta': '<table border="0" class="dataframe table meta-info">\n  <tbody>\n    <tr>\n      <th>turma</th>\n      <td>CARMELIALOFF 4B 2016CARMELIALOFF 4C 2016CARMEL...</td>\n    </tr>\n    <tr>\n      <th>total_alunos</th>\n      <td>71</td>\n    </tr>\n    <tr>\n      <th>usuarios</th>\n      <td>5</td>\n    </tr>\n    <tr>\n      <th>dominam_5_hab</th>\n      <td>2</td>\n    </tr>\n    <tr>\n      <th>inativos</th>\n      <td>65</td>\n    </tr>\n  </tbody>\n</table>',
+                    "html": '<table border="0" class="dataframe table table-bordered detailed">\n  <thead>\n    <tr style="text-align: right;">\n      <th>turma</th>\n      <th>total_alunos</th>\n      <th>usuarios</th>\n      <th>dominam_5_hab</th>\n      <th>inativos</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <td>CARMELIALOFF 4B 2016</td>\n      <td>22</td>\n      <td>2</td>\n      <td>1</td>\n      <td>19</td>\n    </tr>\n    <tr>\n      <td>CARMELIALOFF 4C 2016</td>\n      <td>22</td>\n      <td>3</td>\n      <td>1</td>\n      <td>19</td>\n    </tr>\n    <tr>\n      <td>CARMELIALOFF 5A 2016</td>\n      <td>27</td>\n      <td>0</td>\n      <td>0</td>\n      <td>27</td>\n    </tr>\n  </tbody>\n</table>',
+                }
+            ]
         }
         rendered = template.render(**template_data)
         return rendered
