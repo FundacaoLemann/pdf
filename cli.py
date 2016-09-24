16d8a3c9ae2defb6bfb7ec676ef5cb327aa85640
Wesley Batista <wesleybatistasantos@gmail.com>
2016-09-24 00:38:01 -0300

    Update CLI
    - Apply PEP8 standards
    - Move help string to docstring
    - create a context and pass it to the module

--- cli.py
+++ cli.py
@@ -1,19 +1,43 @@
+import os
 import click
 from PdfGenerator import PdfGenerator
 
-@click.group(help="PDF Reports Generator")
+
+@click.group()
 def cli():
-    return True
+    """
+    "PDF Reports Generator"
+    """
+    pass
+
+
+class Context(object):
+
+    def __init__(self):
+        self.home = os.getcwd()
+
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
+
 
 @cli.command()
 @click.argument('report_code')
 @click.argument('date')
-@click.pass_context
+@pass_context
 def generate(ctx, report_code, date):
-	pdf = PdfGenerator(report_code, date)
-	pdf.generate()
-	output = pdf.get_output_file()
-	click.echo("The pdf file was exported to {}".format(output))
+    """
+    The reports are generated inside a folder with the 'report_code' as \
+    folder name.
+    """
+    params = {
+        'report_code': report_code,
+        'date': date,
+        'path': ctx.home
+    }
+    pdf = PdfGenerator(**params)
+    pdf.generate()
+    output = pdf.get_output_file()
+    click.echo("The pdf file was exported to {}".format(output))
 
 
 if __name__ == '__main__':
