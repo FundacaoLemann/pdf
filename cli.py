cbabf11ee94de5706cff562e4da89b865375b928
fernandosjp <fernando@fundacaolemann.org.br>
2016-09-26 14:16:11 +0000

    add flag to set path of reports folder

--- cli.py
+++ cli.py
@@ -23,8 +23,11 @@ pass_context = click.make_pass_decorator(Context, ensure=True)
 @cli.command()
 @click.argument('report_code')
 @click.argument('date')
+#@click.option('--global_config', default='test')
+@click.option('--report_path', default='')
 @pass_context
-def generate(ctx, report_code, date):
+def generate(ctx, report_code, date, **kwargs):
+
     """
     The reports are generated inside a folder with the 'report_code' as \
     folder name.
@@ -34,7 +37,8 @@ def generate(ctx, report_code, date):
         'date': date,
         'path': ctx.home
     }
-    pdf = PdfGenerator(**params)
+    #click.echo('Testando =%s' % kwargs['global_config'])
+    pdf = PdfGenerator(report_code, date, path=kwargs['report_path'])
     pdf.generate()
     output = pdf.get_output_file()
     click.echo("The pdf file was exported to {}".format(output))
