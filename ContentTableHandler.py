81979ff626a101381422a02b37bc30b8cfb7afc1
Wesley Batista <wesleybatistasantos@gmail.com>
2016-10-24 09:35:54 -0200

    Fix code style in ContentTableHandler

--- ContentTableHandler.py
+++ ContentTableHandler.py
@@ -20,7 +20,7 @@ class ContentTableHandler(object):
         header = df.columns
         rename = {}
         for column in header:
-            rename[column] = (column.title().replace('_',' '))
+            rename[column] = (column.title().replace('_', ' '))
         df.rename(columns=rename, inplace=True)
 
         return df
@@ -30,7 +30,9 @@ class ContentTableHandler(object):
             self.DF = pd.read_csv(self.src, delimiter='|')
         except Exception as e:
             if isinstance(e, IOError):
-                exception = IOError("{}. Please make sure we are using absolute paths".format(e.message))
+                exception = IOError("{}. Please make sure we are using "
+                                    "absolute paths"
+                                    .format(e.message))
             raise exception
 
         self.DF = self._prettify_header(self.DF)
@@ -50,17 +52,18 @@ class ContentTableHandler(object):
         return self.DF_SUM
 
     def _get_df_html(self, df, **kwargs):
-        html_out = df.to_html(**kwargs).replace('border="1"','border="0"')
+        html_out = df.to_html(**kwargs).replace('border="1"', 'border="0"')
         return html_out
 
     def _get_table_html(self):
-        result = self._get_df_html(self.DF, index=False, classes='table table-striped detailed')
+        result = self._get_df_html(self.DF, index=False,
+                                   classes='table table-striped detailed')
         return result
 
     def _get_table_meta_html(self):
-        result = self._get_df_html(self.DF_SUM, header=False, classes='table meta-info')
+        result = self._get_df_html(self.DF_SUM, header=False,
+                                   classes='table meta-info')
         return result
-
 
     def get_table(self, ):
         result = {
